e0ebbe49369188e41b8f24bf4bf5d218638be9f4
John Turner <johnturner4004@gmail.com>
2023-01-18 18:36:08 -0600

    json with data for work history and education

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,20 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   box-sizing: border-box;
+  padding-top: 3.125rem;
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 3.125rem;
+  z-index: 3;
+  background-color: #fff;
+  width: 100vw;
+  box-shadow: 0 5px 10px #222;
+  padding-left: 3.125rem;
+  padding-top: 0.25rem;
 }
 
 .nav__name {
@@ -28,10 +42,12 @@ body {
   line-height: 2.5rem;
   font-weight: 700;
   padding-right: 0.9375rem;
+  color: #000;
 }
 
 .nav__link {
   padding: 0 0.9375rem;
+  color: #000;
   border-left: 1px solid #444;
   font-size: 1.5rem;
   margin: 0.625rem 0;
